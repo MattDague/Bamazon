fe8af1055971d8176fa6c13ae45c15b67d179b1e
Matthew Dague <mattdague@gmail.com>
2019-07-31 16:14:54 -0400

    realized original copy of database schema was overwritten, restored sql file

--- database.sql
+++ database.sql
@@ -1,25 +1,22 @@
-DROP DATABASE playlist_db IF EXISTS;
-
-CREATE DATABASE playlist_db;
-
-use playlist_db;
-
-CREATE TABLE songs (
-  id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(45) NULL,
-  artist VARCHAR(45) NULL,
-  genre VARCHAR(45) NULL,
-  PRIMARY KEY (id)
-);
-
-INSERT INTO songs (title, artist, genre)
-VALUES ("Sanctuary", "Joji", "R&B");
-
-INSERT INTO songs (title, artist, genre)
-VALUES ("Trailer Trash", "Modest Mouse", "Indie");
-
-INSERT INTO songs (title, artist, genre)
-VALUES ("Hurt", "Oliver Tree", "Alternative Rock");
-
-INSERT INTO songs (title, artist, genre)
-VALUES ("Jinji", "Sunset Rollercoaster", "Indie");
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+CREATE TABLE `products` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(30) NOT NULL,
+  `department_name` varchar(30) NOT NULL,
+  `price` decimal(5,2) DEFAULT NULL,
+  `stock_quantity` int(10) DEFAULT NULL,
+  PRIMARY KEY (`item_id`));
+  
+INSERT INTO products (`product_name`,`department_name`,`price`,`stock_quantity`) 
+VALUES ('Headphones','Electronics',24.99,23), 
+('Wok','Cookware',79.97,9), 
+('Bioshock','Video Games',14.00,14),
+('Socks- 10 pack','Fashion',12.45,41),
+('Nice Jeans','Fashion',69.99,15), 
+('A Single Fork','Cookware',1.29,34),
+('World of Warcraft','Video Games',49.99,96),
+('Smart Watch','Electronics',189.49,7),
+('Frying Pan','Cookware',17.89,9),
+('Super Mario Oddyssey','Video Games',59.99,16);
